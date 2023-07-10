658000fb6ae7678c221f8555558e2432695f3c74
Matt Gehrls <mattgehrls@gmail.com>
2023-07-10 12:46:37 -0400

    remove unused import

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/db_provider.dart';
